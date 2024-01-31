e290316f457762ac7e1d3a0945df7bc231e4d8c8
Dmytro Cheholia <d.cheholia@partner.samsung.com>
2024-01-31 14:41:53 +0000

    feat: add age example

---
+++ show_age/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main( void )      {
+    size_t age;
+    std::cout  << "Enter your age: ";
+    std::cin >> age;
+    std::cout << "You are   " <<     age << " years old." << std::endl;
+    return 0;
+ }
