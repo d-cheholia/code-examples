240bc81349c123c6a73e837cd5de5a19d40713d4
Dmytro Cheholia <d.cheholia@partner.samsung.com>
2024-01-31 13:47:41 +0000

    update

--- .universum.py
+++ .universum.py
@@ -5,13 +5,14 @@ import os
 import shutil
 
 
-def create_directories_for_output_files(files: list, root_dir: str, clear = True):
+def create_directories_for_output_files(files: list, root_dir: str, clear=True):
     """
     Create directories for output files.
     There is temporary solution to avoid errors when using universum.analyzers.clang_format
     """
     for file in files:
         Path(root_dir, file).parent.mkdir(parents=True, exist_ok=True)
+
 
 def get_all_files_in_dir(path: str, extensions: list):
     """
@@ -21,26 +22,16 @@ def get_all_files_in_dir(path: str, extensions: list):
     return [file for ext in extensions for file in Path(path).rglob(f"*.{ext}")]
 
 
-
 # change directory to temp
-
-# current_dir = os.getcwd()
-# os.chdir("./temp")
 found_files = get_all_files_in_dir(".", ["cpp", "c"])
 found_files = [str(file) for file in found_files if "temp" not in str(file)]
 
 print(found_files)
 
-
 if found_files:
-    root_dir =  "clang_report/"
+    root_dir = "clang_report/"
     create_directories_for_output_files(found_files, root_dir=root_dir)
     os.environ["ENABLE_CLANG_FORMAT"] = "1"
-
-# change directory back
-# os.chdir(current_dir)
-    
-print (os.getcwd())
 
 configs = Configuration(
     [
@@ -62,10 +53,7 @@ configs = Configuration(
                 "--output-directory",
                 "clang_report",
             ],
-            directory="."
         ),
-
-
     ]
 )
 
